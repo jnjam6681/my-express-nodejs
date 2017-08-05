51fcfdd934ea53264084eb2f1eb5237b7bfe6296
jnjam6681 <jnjam_pk6681@hotmail.com>
2017-08-06 01:27:01 +0700

    express

---
+++ spec/my-express-nodejs-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import MyExpressNodejsView from '../lib/my-express-nodejs-view';
+
+describe('MyExpressNodejsView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
